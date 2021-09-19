825f0c8b6288f9c14672b1d0370facd66faa848a
Eugene <jesthete78@gmail.com>
2021-09-19 20:14:52 +0300

    add rigth sintaxys of the object

--- js/script.js
+++ js/script.js
@@ -18,11 +18,10 @@ rate1 = prompt("Как оцените его?", "");
 movie2 = prompt("Один из просмотренных фильмов", "");
 rate2 = prompt("Как оцените его?", "");
 
-personalMovies.movies.movie1 = rate1;
-personalMovies.movies.movie2 = rate2;
+personalMovies.movies[movie1] = rate1;
+personalMovies.movies[movie2] = rate2;
 
-console.log(personalMovies.movies.movie1);
-console.log(personalMovies.movies.movie2);
+console.log(personalMovies);
 
 
 
