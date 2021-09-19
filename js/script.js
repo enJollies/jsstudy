d9b8a59d2f4e640b0cb0218570c9511c23110275
Eugene <jesthete78@gmail.com>
2021-09-19 20:05:38 +0300

    add quetions and output answers

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", ""),
 	movies = {},
 	actors = {},
-	genres = [];
+	genres = [],
+	answers = [];
 let personalMovies = {
 	count: numberOfFilms,
 	movies: movies,
@@ -9,4 +10,19 @@ let personalMovies = {
 	genres: genres,
 	privat: false
 };
+
+let movie1, movie2, rate1, rate2;
+
+movie1 = prompt("Один из просмотренных фильмов", "");
+rate1 = prompt("Как оцените его?", "");
+movie2 = prompt("Один из просмотренных фильмов", "");
+rate2 = prompt("Как оцените его?", "");
+
+personalMovies.movies.movie1 = rate1;
+personalMovies.movies.movie2 = rate2;
+
+console.log(personalMovies.movies.movie1);
+console.log(personalMovies.movies.movie2);
+
+
 
