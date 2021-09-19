17c9a0244a9cdc88640c69838ebf677febd2bfc2
Eugene <jesthete78@gmail.com>
2021-09-19 19:23:45 +0300

    add personalmovies object

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", ""),
+	movies = {},
+	actors = {},
+	genres = [];
+let personalMovies = {
+	count: numberOfFilms,
+	movies: movies,
+	actors: actors,
+	genres: genres,
+	privat: false
+}
